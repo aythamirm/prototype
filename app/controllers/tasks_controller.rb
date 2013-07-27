18f4d0fd2f0df68fd2faa0ca57e27f9344e2a70a
aythamirm <aythamirm@gmail.com>
2013-07-27 15:21:43 +0100

    Ignore2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
       if params[:filter][:search] != ""
         @nodes = current_user.tasks.where(task_name: params[:filter][:search])  
       end
-      # estoy trabajando aquí m
+      # estoy trabajando aquí mm
       if params[:state][:paused].paused
         @nodes = current_user.tasks.where(state: params[:filter][:paused])
       end  
