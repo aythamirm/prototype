b23dae210e5f9a19d3e85d7e8074f395a45d41de
aythamirm <aythamirm@gmail.com>
2013-11-07 23:03:31 +0000

    Fixed bugs with user avatar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :unproductive_time, :total_time)
   end
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :avatar, :unproductive_time, :total_time)
   end
   private :sign_up_params
   private :account_update_params
